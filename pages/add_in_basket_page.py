bc74a7de31e78fdb3c497f7d03a811480cb0ea27
DaryaKondratovich <darya.kondratovich@gmail.com>
2022-05-15 17:06:33 +0300

    NEW page add in basket and parametrize links

---
+++ pages/add_in_basket_page.py
@@ -0,0 +1,35 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import AddInBasket
+
+class AddInBasketPage(BasePage):
+    def add_in_basket(self):
+        basket = self.browser.find_element(*AddInBasket.ADD_IN_BASKET)
+        basket.click()
+
+    def message_about_add_in_basket(self):
+        assert self.is_element_present(*AddInBasket.MESSAGE_ABOUT_ADD_IN_BASKET)
+
+    def name_book(self):
+        name_in_message = self.browser.find_element(*AddInBasket.MESSAGE_NAME_BOOK)
+        main_name_book = self.browser.find_element(*AddInBasket.NAME_BOOK)
+        assert name_in_message.text == main_name_book.text, 'Name a book in message != name main a book'  
+
+    def price(self):
+        price_in_basket = self.browser.find_element(*AddInBasket.PRICE_BASKET)
+        main_price = self.browser.find_element(*AddInBasket.PRICE_BOOK)
+        assert price_in_basket.text == main_price.text, 'Price in a basket != main a price'     
+
+  
+
+            
+
+
+
+
+
+
+
+
+
+
--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,15 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-class LoginPageLocators ():
+class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")    
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")   
+
+class AddInBasket():
+    ADD_IN_BASKET = (By.CSS_SELECTOR, "button.btn.btn-lg.btn-primary.btn-add-to-basket")
+    MESSAGE_ABOUT_ADD_IN_BASKET = (By.CSS_SELECTOR, '[style="visibility: visible;"] :nth-child(1)')
+    MESSAGE_NAME_BOOK = (By.CSS_SELECTOR, '[style="visibility: visible;"] :nth-child(1) :nth-child(2) :nth-child(1)')
+    NAME_BOOK = (By.CSS_SELECTOR, 'div.col-sm-6.product_main :nth-child(1)')
+    PRICE_BASKET = (By.CSS_SELECTOR, "div.alert.alert-safe.alert-noicon.alert-info :nth-child(2) :nth-child(1) :nth-child(1)")
+    PRICE_BOOK = (By.CSS_SELECTOR, 'div.col-sm-6.product_main :nth-child(2)')
+
